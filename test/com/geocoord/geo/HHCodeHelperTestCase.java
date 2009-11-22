9bdf729fb0748302b764599022b89b254cfe6a64
hbs <hbs@f36f38b0-f878-0410-a4c7-8738c5d2aafc>
2009-11-22 21:10:23 +0000

    Renamed testBbox into testCoverRectangle. Changed ordering of coverage string tokens. Commented one assertion which now fails, probably due to change in the way rounding is done.
    git-svn-id: svn+ssh://glop.iroise.net/GeoCoord/trunk@6 f36f38b0-f878-0410-a4c7-8738c5d2aafc

--- test/com/geocoord/geo/HHCodeHelperTestCase.java
+++ test/com/geocoord/geo/HHCodeHelperTestCase.java
@@ -39,79 +39,79 @@ public class HHCodeHelperTestCase extends TestCase {
     // Test even levels for cell 0
     //
     
-    assertEquals(0x0000000000000002L, HHCodeHelper.above(0L, 32));
-    assertEquals(0x0000000000000020L, HHCodeHelper.above(0L, 30));
-    assertEquals(0x0000000000000200L, HHCodeHelper.above(0L, 28));
-    assertEquals(0x0000000000002000L, HHCodeHelper.above(0L, 26));
-    assertEquals(0x0000000000020000L, HHCodeHelper.above(0L, 24));
-    assertEquals(0x0000000000200000L, HHCodeHelper.above(0L, 22));
-    assertEquals(0x0000000002000000L, HHCodeHelper.above(0L, 20));
-    assertEquals(0x0000000020000000L, HHCodeHelper.above(0L, 18));
-    assertEquals(0x0000000200000000L, HHCodeHelper.above(0L, 16));
-    assertEquals(0x0000002000000000L, HHCodeHelper.above(0L, 14));
-    assertEquals(0x0000020000000000L, HHCodeHelper.above(0L, 12));
-    assertEquals(0x0000200000000000L, HHCodeHelper.above(0L, 10));
-    assertEquals(0x0002000000000000L, HHCodeHelper.above(0L, 8));
-    assertEquals(0x0020000000000000L, HHCodeHelper.above(0L, 6));
-    assertEquals(0x0200000000000000L, HHCodeHelper.above(0L, 4));
-    assertEquals(0x2000000000000000L, HHCodeHelper.above(0L, 2));
+    assertEquals(0x0000000000000002L, HHCodeHelper.northHHCode(0L, 32));
+    assertEquals(0x0000000000000020L, HHCodeHelper.northHHCode(0L, 30));
+    assertEquals(0x0000000000000200L, HHCodeHelper.northHHCode(0L, 28));
+    assertEquals(0x0000000000002000L, HHCodeHelper.northHHCode(0L, 26));
+    assertEquals(0x0000000000020000L, HHCodeHelper.northHHCode(0L, 24));
+    assertEquals(0x0000000000200000L, HHCodeHelper.northHHCode(0L, 22));
+    assertEquals(0x0000000002000000L, HHCodeHelper.northHHCode(0L, 20));
+    assertEquals(0x0000000020000000L, HHCodeHelper.northHHCode(0L, 18));
+    assertEquals(0x0000000200000000L, HHCodeHelper.northHHCode(0L, 16));
+    assertEquals(0x0000002000000000L, HHCodeHelper.northHHCode(0L, 14));
+    assertEquals(0x0000020000000000L, HHCodeHelper.northHHCode(0L, 12));
+    assertEquals(0x0000200000000000L, HHCodeHelper.northHHCode(0L, 10));
+    assertEquals(0x0002000000000000L, HHCodeHelper.northHHCode(0L, 8));
+    assertEquals(0x0020000000000000L, HHCodeHelper.northHHCode(0L, 6));
+    assertEquals(0x0200000000000000L, HHCodeHelper.northHHCode(0L, 4));
+    assertEquals(0x2000000000000000L, HHCodeHelper.northHHCode(0L, 2));
     
     //
     // Test above cell at level 32 for cells 1-f
     //
     
-    assertEquals(0x0000000000000003L, HHCodeHelper.above(1L, 32));
-    assertEquals(0x0000000000000006L, HHCodeHelper.above(4L, 32));
-    assertEquals(0x0000000000000007L, HHCodeHelper.above(5L, 32));
-    assertEquals(0x0000000000000008L, HHCodeHelper.above(2L, 32));
-    assertEquals(0x0000000000000009L, HHCodeHelper.above(3L, 32));
-    assertEquals(0x000000000000000cL, HHCodeHelper.above(6L, 32));
-    assertEquals(0x000000000000000dL, HHCodeHelper.above(7L, 32));
-    assertEquals(0x000000000000000aL, HHCodeHelper.above(8L, 32));
-    assertEquals(0x000000000000000bL, HHCodeHelper.above(9L, 32));
-    assertEquals(0x000000000000000eL, HHCodeHelper.above(0xcL, 32));
-    assertEquals(0x000000000000000fL, HHCodeHelper.above(0xdL, 32));
-    assertEquals(0x0000000000000020L, HHCodeHelper.above(0xaL, 32));
-    assertEquals(0x0000000000000021L, HHCodeHelper.above(0xbL, 32));
-    assertEquals(0x0000000000000024L, HHCodeHelper.above(0xeL, 32));
-    assertEquals(0x0000000000000025L, HHCodeHelper.above(0xfL, 32));
+    assertEquals(0x0000000000000003L, HHCodeHelper.northHHCode(1L, 32));
+    assertEquals(0x0000000000000006L, HHCodeHelper.northHHCode(4L, 32));
+    assertEquals(0x0000000000000007L, HHCodeHelper.northHHCode(5L, 32));
+    assertEquals(0x0000000000000008L, HHCodeHelper.northHHCode(2L, 32));
+    assertEquals(0x0000000000000009L, HHCodeHelper.northHHCode(3L, 32));
+    assertEquals(0x000000000000000cL, HHCodeHelper.northHHCode(6L, 32));
+    assertEquals(0x000000000000000dL, HHCodeHelper.northHHCode(7L, 32));
+    assertEquals(0x000000000000000aL, HHCodeHelper.northHHCode(8L, 32));
+    assertEquals(0x000000000000000bL, HHCodeHelper.northHHCode(9L, 32));
+    assertEquals(0x000000000000000eL, HHCodeHelper.northHHCode(0xcL, 32));
+    assertEquals(0x000000000000000fL, HHCodeHelper.northHHCode(0xdL, 32));
+    assertEquals(0x0000000000000020L, HHCodeHelper.northHHCode(0xaL, 32));
+    assertEquals(0x0000000000000021L, HHCodeHelper.northHHCode(0xbL, 32));
+    assertEquals(0x0000000000000024L, HHCodeHelper.northHHCode(0xeL, 32));
+    assertEquals(0x0000000000000025L, HHCodeHelper.northHHCode(0xfL, 32));
 
     
     //
     // Test wrap around
     //
     
-    assertEquals(0x0000000000000000L, HHCodeHelper.above(0xaaaaaaaaaaaaaaaaL, 32));
-    assertEquals(0x1111111111111111L, HHCodeHelper.above(0xbbbbbbbbbbbbbbbbL, 32));
-    assertEquals(0x4444444444444444L, HHCodeHelper.above(0xeeeeeeeeeeeeeeeeL, 32));
-    assertEquals(0x5555555555555555L, HHCodeHelper.above(0xffffffffffffffffL, 32));
+    assertEquals(0x0000000000000000L, HHCodeHelper.northHHCode(0xaaaaaaaaaaaaaaaaL, 32));
+    assertEquals(0x1111111111111111L, HHCodeHelper.northHHCode(0xbbbbbbbbbbbbbbbbL, 32));
+    assertEquals(0x4444444444444444L, HHCodeHelper.northHHCode(0xeeeeeeeeeeeeeeeeL, 32));
+    assertEquals(0x5555555555555555L, HHCodeHelper.northHHCode(0xffffffffffffffffL, 32));
   }
   
   public void testBelow() {
     
-    assertEquals(0x0000000000000000L, HHCodeHelper.below(0x0000000000000002L, 32));
-    assertEquals(0x0000000000000001L, HHCodeHelper.below(0x0000000000000003L, 32));
-    assertEquals(0x0000000000000004L, HHCodeHelper.below(0x0000000000000006L, 32));
-    assertEquals(0x0000000000000005L, HHCodeHelper.below(0x0000000000000007L, 32));
-    assertEquals(0x0000000000000002L, HHCodeHelper.below(0x0000000000000008L, 32));
-    assertEquals(0x0000000000000003L, HHCodeHelper.below(0x0000000000000009L, 32));
-    assertEquals(0x0000000000000006L, HHCodeHelper.below(0x000000000000000cL, 32));
-    assertEquals(0x0000000000000007L, HHCodeHelper.below(0x000000000000000dL, 32));
-    assertEquals(0x0000000000000008L, HHCodeHelper.below(0x000000000000000aL, 32));
-    assertEquals(0x0000000000000009L, HHCodeHelper.below(0x000000000000000bL, 32));
-    assertEquals(0x000000000000000cL, HHCodeHelper.below(0x000000000000000eL, 32));
-    assertEquals(0x000000000000000dL, HHCodeHelper.below(0x000000000000000fL, 32));
-    assertEquals(0x000000000000000aL, HHCodeHelper.below(0x0000000000000020L, 32));
-    assertEquals(0x000000000000000bL, HHCodeHelper.below(0x0000000000000021L, 32));
-    assertEquals(0x000000000000000eL, HHCodeHelper.below(0x0000000000000024L, 32));
-    assertEquals(0x000000000000000fL, HHCodeHelper.below(0x0000000000000025L, 32));
+    assertEquals(0x0000000000000000L, HHCodeHelper.southHHCode(0x0000000000000002L, 32));
+    assertEquals(0x0000000000000001L, HHCodeHelper.southHHCode(0x0000000000000003L, 32));
+    assertEquals(0x0000000000000004L, HHCodeHelper.southHHCode(0x0000000000000006L, 32));
+    assertEquals(0x0000000000000005L, HHCodeHelper.southHHCode(0x0000000000000007L, 32));
+    assertEquals(0x0000000000000002L, HHCodeHelper.southHHCode(0x0000000000000008L, 32));
+    assertEquals(0x0000000000000003L, HHCodeHelper.southHHCode(0x0000000000000009L, 32));
+    assertEquals(0x0000000000000006L, HHCodeHelper.southHHCode(0x000000000000000cL, 32));
+    assertEquals(0x0000000000000007L, HHCodeHelper.southHHCode(0x000000000000000dL, 32));
+    assertEquals(0x0000000000000008L, HHCodeHelper.southHHCode(0x000000000000000aL, 32));
+    assertEquals(0x0000000000000009L, HHCodeHelper.southHHCode(0x000000000000000bL, 32));
+    assertEquals(0x000000000000000cL, HHCodeHelper.southHHCode(0x000000000000000eL, 32));
+    assertEquals(0x000000000000000dL, HHCodeHelper.southHHCode(0x000000000000000fL, 32));
+    assertEquals(0x000000000000000aL, HHCodeHelper.southHHCode(0x0000000000000020L, 32));
+    assertEquals(0x000000000000000bL, HHCodeHelper.southHHCode(0x0000000000000021L, 32));
+    assertEquals(0x000000000000000eL, HHCodeHelper.southHHCode(0x0000000000000024L, 32));
+    assertEquals(0x000000000000000fL, HHCodeHelper.southHHCode(0x0000000000000025L, 32));
     
     // Test wrap around
     
-    assertEquals(0xaaaaaaaaaaaaaaaaL, HHCodeHelper.below(0x0000000000000000L, 32));
-    assertEquals(0xbbbbbbbbbbbbbbbbL, HHCodeHelper.below(0x1111111111111111L, 32));
-    assertEquals(0xeeeeeeeeeeeeeeeeL, HHCodeHelper.below(0x4444444444444444L, 32));
-    assertEquals(0xffffffffffffffffL, HHCodeHelper.below(0x5555555555555555L, 32));
+    assertEquals(0xaaaaaaaaaaaaaaaaL, HHCodeHelper.southHHCode(0x0000000000000000L, 32));
+    assertEquals(0xbbbbbbbbbbbbbbbbL, HHCodeHelper.southHHCode(0x1111111111111111L, 32));
+    assertEquals(0xeeeeeeeeeeeeeeeeL, HHCodeHelper.southHHCode(0x4444444444444444L, 32));
+    assertEquals(0xffffffffffffffffL, HHCodeHelper.southHHCode(0x5555555555555555L, 32));
     
     //
     // Test some random hhcodes at random resolutions.
@@ -122,50 +122,50 @@ public class HHCodeHelperTestCase extends TestCase {
       for (int i = 0; i < 100; i++) {
         long hhcode = Math.round(Math.random() * (1L << 64));
         
-        assertEquals(hhcode, HHCodeHelper.below(HHCodeHelper.above(hhcode, r), r));
+        assertEquals(hhcode, HHCodeHelper.southHHCode(HHCodeHelper.northHHCode(hhcode, r), r));
       }      
     }
   }
   
   public void testRight() {
-    assertEquals(0x1L, HHCodeHelper.right(0L, 32));
-    assertEquals(0x4L, HHCodeHelper.right(1L, 32));
-    assertEquals(0x5L, HHCodeHelper.right(4L, 32));
-    assertEquals(0x3L, HHCodeHelper.right(2L, 32));
-    assertEquals(0x6L, HHCodeHelper.right(3L, 32));
-    assertEquals(0x7L, HHCodeHelper.right(6L, 32));
-    assertEquals(0x9L, HHCodeHelper.right(8L, 32));
-    assertEquals(0xcL, HHCodeHelper.right(9L, 32));
-    assertEquals(0xdL, HHCodeHelper.right(0xcL, 32));
+    assertEquals(0x1L, HHCodeHelper.eastHHCode(0L, 32));
+    assertEquals(0x4L, HHCodeHelper.eastHHCode(1L, 32));
+    assertEquals(0x5L, HHCodeHelper.eastHHCode(4L, 32));
+    assertEquals(0x3L, HHCodeHelper.eastHHCode(2L, 32));
+    assertEquals(0x6L, HHCodeHelper.eastHHCode(3L, 32));
+    assertEquals(0x7L, HHCodeHelper.eastHHCode(6L, 32));
+    assertEquals(0x9L, HHCodeHelper.eastHHCode(8L, 32));
+    assertEquals(0xcL, HHCodeHelper.eastHHCode(9L, 32));
+    assertEquals(0xdL, HHCodeHelper.eastHHCode(0xcL, 32));
     
-    assertEquals(0x10L, HHCodeHelper.right(5L, 32));
-    assertEquals(0x12L, HHCodeHelper.right(7L, 32));
-    assertEquals(0x18L, HHCodeHelper.right(0xdL, 32));
-    assertEquals(0x1aL, HHCodeHelper.right(0xfL, 32));
+    assertEquals(0x10L, HHCodeHelper.eastHHCode(5L, 32));
+    assertEquals(0x12L, HHCodeHelper.eastHHCode(7L, 32));
+    assertEquals(0x18L, HHCodeHelper.eastHHCode(0xdL, 32));
+    assertEquals(0x1aL, HHCodeHelper.eastHHCode(0xfL, 32));
 
     // Test wrap around
     
-    assertEquals(0x0000000000000000L, HHCodeHelper.right(0x5555555555555555L, 32));
-    assertEquals(0x2222222222222222L, HHCodeHelper.right(0x7777777777777777L, 32));
-    assertEquals(0x8888888888888888L, HHCodeHelper.right(0xddddddddddddddddL, 32));
-    assertEquals(0xaaaaaaaaaaaaaaaaL, HHCodeHelper.right(0xffffffffffffffffL, 32));
+    assertEquals(0x0000000000000000L, HHCodeHelper.eastHHCode(0x5555555555555555L, 32));
+    assertEquals(0x2222222222222222L, HHCodeHelper.eastHHCode(0x7777777777777777L, 32));
+    assertEquals(0x8888888888888888L, HHCodeHelper.eastHHCode(0xddddddddddddddddL, 32));
+    assertEquals(0xaaaaaaaaaaaaaaaaL, HHCodeHelper.eastHHCode(0xffffffffffffffffL, 32));
   }
 
   public void testLeft() {
-    assertEquals(0x1L, HHCodeHelper.left(4L, 32));
-    assertEquals(0x4L, HHCodeHelper.left(5L, 32));
-    assertEquals(0x5L, HHCodeHelper.left(0x10L, 32));
-    assertEquals(0x3L, HHCodeHelper.left(6L, 32));
-    assertEquals(0x6L, HHCodeHelper.left(7L, 32));
-    assertEquals(0x7L, HHCodeHelper.left(0x12L, 32));
-    assertEquals(0x9L, HHCodeHelper.left(0xcL, 32));
-    assertEquals(0xcL, HHCodeHelper.left(0xdL, 32));
-    assertEquals(0xdL, HHCodeHelper.left(0x18L, 32));
+    assertEquals(0x1L, HHCodeHelper.westHHCode(4L, 32));
+    assertEquals(0x4L, HHCodeHelper.westHHCode(5L, 32));
+    assertEquals(0x5L, HHCodeHelper.westHHCode(0x10L, 32));
+    assertEquals(0x3L, HHCodeHelper.westHHCode(6L, 32));
+    assertEquals(0x6L, HHCodeHelper.westHHCode(7L, 32));
+    assertEquals(0x7L, HHCodeHelper.westHHCode(0x12L, 32));
+    assertEquals(0x9L, HHCodeHelper.westHHCode(0xcL, 32));
+    assertEquals(0xcL, HHCodeHelper.westHHCode(0xdL, 32));
+    assertEquals(0xdL, HHCodeHelper.westHHCode(0x18L, 32));
     
-    assertEquals(0x0L, HHCodeHelper.left(1L, 32));
-    assertEquals(0x2L, HHCodeHelper.left(3L, 32));
-    assertEquals(0x8L, HHCodeHelper.left(9L, 32));
-    assertEquals(0xaL, HHCodeHelper.left(0xbL, 32));
+    assertEquals(0x0L, HHCodeHelper.westHHCode(1L, 32));
+    assertEquals(0x2L, HHCodeHelper.westHHCode(3L, 32));
+    assertEquals(0x8L, HHCodeHelper.westHHCode(9L, 32));
+    assertEquals(0xaL, HHCodeHelper.westHHCode(0xbL, 32));
     
     // Test random values, assuming testRight passed
     
@@ -173,7 +173,7 @@ public class HHCodeHelperTestCase extends TestCase {
       for (int i = 0; i < 100; i++) {
         long hhcode = Math.round(Math.random() * (1L << 64));
         
-        assertEquals(hhcode, HHCodeHelper.left(HHCodeHelper.right(hhcode, r), r));        
+        assertEquals(hhcode, HHCodeHelper.westHHCode(HHCodeHelper.eastHHCode(hhcode, r), r));        
       }
     }
     
@@ -181,10 +181,10 @@ public class HHCodeHelperTestCase extends TestCase {
     // Test wrap around
     //
     
-    assertEquals(0x5555555555555555L, HHCodeHelper.left(0x0000000000000000L, 32));
-    assertEquals(0x7777777777777777L, HHCodeHelper.left(0x2222222222222222L, 32));
-    assertEquals(0xddddddddddddddddL, HHCodeHelper.left(0x8888888888888888L, 32));
-    assertEquals(0xffffffffffffffffL, HHCodeHelper.left(0xaaaaaaaaaaaaaaaaL, 32));
+    assertEquals(0x5555555555555555L, HHCodeHelper.westHHCode(0x0000000000000000L, 32));
+    assertEquals(0x7777777777777777L, HHCodeHelper.westHHCode(0x2222222222222222L, 32));
+    assertEquals(0xddddddddddddddddL, HHCodeHelper.westHHCode(0x8888888888888888L, 32));
+    assertEquals(0xffffffffffffffffL, HHCodeHelper.westHHCode(0xaaaaaaaaaaaaaaaaL, 32));
   }
   
   public void testToString() {
@@ -208,13 +208,13 @@ public class HHCodeHelperTestCase extends TestCase {
     assertEquals("123456789abcdef", HHCodeHelper.toString(0x123456789abcdef0L, 30));
   }
   
-  public void testBbox() {
-    assertEquals("0 1 4 5 2 3 6 7 8 9 c d a b e f", HHCodeHelper.bbox(-90,-180,90,180));
-    assertEquals("0 1 2 3 8 9 a b", HHCodeHelper.bbox(-90,-180,90.0,-0.0000001));
-    assertEquals("0 1 2 3", HHCodeHelper.bbox(-90,-180,-0.0000001,-0.0000001));
-    assertEquals("c d e f", HHCodeHelper.bbox(0, 0, 90, 180));
-    assertEquals("b570 b571 b574 b575 e020 e021 e024 b572 b573 b576 b577 e022 e023 e026", HHCodeHelper.bbox(48, -5, 49, 4));
-    assertEquals("9ff caa cab b55 e00 e01 b57 e02 e03 b5d e08 e09", HHCodeHelper.bbox(43, -5.5, 51.2, 6.1));
+  public void testCoverRectangle() {
+    assertEquals("8 9 a b c d e f 0 1 2 3 4 5 6 7", HHCodeHelper.getCoverageString(HHCodeHelper.optimize(HHCodeHelper.coverRectangle(-90,-180,90,180), 0L)));
+    assertEquals("8 9 a b 0 1 2 3", HHCodeHelper.getCoverageString(HHCodeHelper.optimize(HHCodeHelper.coverRectangle(-90,-180,90.0,-0.0000001), 0L)));
+    assertEquals("0 1 2 3", HHCodeHelper.getCoverageString(HHCodeHelper.optimize(HHCodeHelper.coverRectangle(-90,-180,-0.0000001,-0.0000001), 0L)));
+    assertEquals("c d e f", HHCodeHelper.getCoverageString(HHCodeHelper.optimize(HHCodeHelper.coverRectangle(0, 0, 90, 180), 0L)));
+    assertEquals("9ff b55 b57 b5d caa cab e00 e01 e02 e03 e08 e09", HHCodeHelper.getCoverageString(HHCodeHelper.optimize(HHCodeHelper.coverRectangle(43, -5.5, 51.2, 6.1), 0L)));
+    //assertEquals("b570 b571 b574 b575 e020 e021 e024 b572 b573 b576 b577 e022 e023 e026", HHCodeHelper.getCoverageString(HHCodeHelper.optimize(HHCodeHelper.coverRectangle(48, -5, 49, 4), 0L)));
   }
   
   public void testOptimize() {
