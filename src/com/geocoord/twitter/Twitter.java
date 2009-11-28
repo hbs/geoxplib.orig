4b7394bd30d0fcf1ea2e516e03226839f4aed1ac
hbs <hbs@f36f38b0-f878-0410-a4c7-8738c5d2aafc>
2009-11-28 09:51:17 +0000

    Initial revision of Twitter packge.
    git-svn-id: svn+ssh://glop.iroise.net/GeoCoord/trunk@18 f36f38b0-f878-0410-a4c7-8738c5d2aafc

---
+++ src/com/geocoord/twitter/Twitter.java
@@ -0,0 +1,10 @@
+package com.geocoord.twitter;
+
+public class Twitter {
+  public static final String CONSUMER_KEY = "e3D1Liq8H8REuD7JniQUQ";
+  public static final String CONSUMER_SECRET = "WMmBYwESuPbBiO0QE8elKt1VXosFBBEX3pQjDHxYEA";
+  public static final String REQUEST_TOKEN_URL = "http://twitter.com/oauth/request_token";
+  public static final String ACCESS_TOKEN_URL = "http://twitter.com/oauth/access_token";
+  public static final String AUTHORIZE_URL = "http://twitter.com/oauth/authorize";
+  
+}
