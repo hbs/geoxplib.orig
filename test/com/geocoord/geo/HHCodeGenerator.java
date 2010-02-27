72045fa6aa37408734d3f6cf0d8925563f097153
hbs <hbs@f36f38b0-f878-0410-a4c7-8738c5d2aafc>
2010-02-27 22:43:38 +0000

    Initial revison of centroid related code.
    git-svn-id: svn+ssh://glop.iroise.net/GeoCoord/trunk@54 f36f38b0-f878-0410-a4c7-8738c5d2aafc

---
+++ test/com/geocoord/geo/HHCodeGenerator.java
@@ -0,0 +1,23 @@
+package com.geocoord.geo;
+
+import java.security.SecureRandom;
+
+public class HHCodeGenerator {
+  public static void main(String[] args) throws Exception {
+    
+    long count = Long.valueOf(args[0]);
+    
+    SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+    
+    StringBuilder sb = new StringBuilder();
+    
+    for (int i = 0; i < count; i++) {
+      sb.setLength(0);
+      sb.append(Long.toHexString(sr.nextLong()));
+      while(sb.length() < 16) {
+        sb.insert(0, "0");
+      }
+      System.out.println(sb.toString());
+    }
+  }
+}
