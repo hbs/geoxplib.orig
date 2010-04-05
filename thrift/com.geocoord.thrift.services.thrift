9715dc1eab0315a9d708af901c07d1838a369ddb
hbs <hbs@f36f38b0-f878-0410-a4c7-8738c5d2aafc>
2010-04-05 22:30:02 +0000

    Removed inheritance from PoolableThriftService after application of THRIFT-746. Needs a compiler with this patch applied for GeoXP to compile properly.
    git-svn-id: svn+ssh://glop.iroise.net/GeoCoord/trunk@154 f36f38b0-f878-0410-a4c7-8738c5d2aafc

--- thrift/com.geocoord.thrift.services.thrift
+++ thrift/com.geocoord.thrift.services.thrift
@@ -2,22 +2,18 @@ namespace java com.geocoord.thrift.services
 
 include "com.geocoord.thrift.data.thrift"
 
-service PoolableThriftService {
-  //  i32 healthcheck()
-}
-
-service CoverageService extends PoolableThriftService {
+service CoverageService {
   com.geocoord.thrift.data.CoverageResponse getCoverage(1:com.geocoord.thrift.data.CoverageRequest request)  
 }
 
-service DataService extends PoolableThriftService {
+service DataService {
   //com.geocoord.thrift.data.DataResponse lookup(1:com.geocoord.thrift.data.DataRequest) throws (1:com.geocoord.thrift.data.GeoCoordException e)
 }
 
-service SearchService extends PoolableThriftService {
+service SearchService {
 }
 
-service UserService extends PoolableThriftService {
+service UserService {
   com.geocoord.thrift.data.UserCreateResponse   create(1:com.geocoord.thrift.data.UserCreateRequest request) throws (1:com.geocoord.thrift.data.GeoCoordException e)
   com.geocoord.thrift.data.UserAliasResponse    alias(1:com.geocoord.thrift.data.UserAliasRequest request) throws (1:com.geocoord.thrift.data.GeoCoordException e)
   com.geocoord.thrift.data.UserRetrieveResponse retrieve(1:com.geocoord.thrift.data.UserRetrieveRequest request) throws (1:com.geocoord.thrift.data.GeoCoordException e)    
@@ -27,14 +23,14 @@ service UserService extends PoolableThriftService {
 /**
  * Service used to access Layer objects persistently stored.
  */
-service LayerService extends PoolableThriftService {
+service LayerService {
   com.geocoord.thrift.data.LayerCreateResponse   create(1:com.geocoord.thrift.data.LayerCreateRequest request) throws (1:com.geocoord.thrift.data.GeoCoordException e)
   com.geocoord.thrift.data.LayerRetrieveResponse retrieve(1:com.geocoord.thrift.data.LayerRetrieveRequest request) throws (1:com.geocoord.thrift.data.GeoCoordException e)
   com.geocoord.thrift.data.LayerUpdateResponse   update(1:com.geocoord.thrift.data.LayerUpdateRequest request) throws (1:com.geocoord.thrift.data.GeoCoordException e)
   com.geocoord.thrift.data.LayerRemoveResponse   remove(1:com.geocoord.thrift.data.LayerRemoveRequest request) throws (1:com.geocoord.thrift.data.GeoCoordException e)      
 }
 
-service AtomService extends PoolableThriftService {
+service AtomService {
   com.geocoord.thrift.data.AtomCreateResponse   create(1:com.geocoord.thrift.data.AtomCreateRequest request) throws (1:com.geocoord.thrift.data.GeoCoordException e)
   com.geocoord.thrift.data.AtomRetrieveResponse retrieve(1:com.geocoord.thrift.data.AtomRetrieveRequest request) throws (1:com.geocoord.thrift.data.GeoCoordException e)
   com.geocoord.thrift.data.AtomUpdateResponse   update(1:com.geocoord.thrift.data.AtomUpdateRequest request) throws (1:com.geocoord.thrift.data.GeoCoordException e)
@@ -49,6 +45,6 @@ service ActivityService {
   void record(1:com.geocoord.thrift.data.ActivityEvent event) throws (1:com.geocoord.thrift.data.GeoCoordException e)
 }
 
-service CentroidService extends PoolableThriftService {
+service CentroidService {
   com.geocoord.thrift.data.CentroidResponse search(1:com.geocoord.thrift.data.CentroidRequest request) throws (1:com.geocoord.thrift.data.GeoCoordException e)
 }
