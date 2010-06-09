ddcc0181fc3d1c41a5a526a6712e24f4c2ec6c6b
hbs <hbs@f36f38b0-f878-0410-a4c7-8738c5d2aafc>
2010-06-09 21:02:05 +0000

    Initial revision of SearchServlet
    git-svn-id: svn+ssh://glop.iroise.net/GeoCoord/trunk@234 f36f38b0-f878-0410-a4c7-8738c5d2aafc

---
+++ src/com/geocoord/server/servlet/api/SearchServlet.java
@@ -0,0 +1,10 @@
+package com.geocoord.server.servlet.api;
+
+import javax.servlet.http.HttpServlet;
+
+import com.google.inject.Singleton;
+
+@Singleton
+public class SearchServlet extends HttpServlet {
+  
+}
