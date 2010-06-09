6a69a2c3be2a1941752d5bbed1878c8967a5052b
hbs <hbs@f36f38b0-f878-0410-a4c7-8738c5d2aafc>
2010-06-09 21:03:24 +0000

    Added atom/search servlet mappings.
    git-svn-id: svn+ssh://glop.iroise.net/GeoCoord/trunk@237 f36f38b0-f878-0410-a4c7-8738c5d2aafc

--- src/com/geocoord/server/servlet/GuiceServletModule.java
+++ src/com/geocoord/server/servlet/GuiceServletModule.java
@@ -1,6 +1,8 @@
 package com.geocoord.server.servlet;
 
+import com.geocoord.server.servlet.api.AtomServlet;
 import com.geocoord.server.servlet.api.LayerServlet;
+import com.geocoord.server.servlet.api.SearchServlet;
 import com.google.inject.servlet.ServletModule;
 
 public class GuiceServletModule extends ServletModule {
@@ -8,5 +10,8 @@ public class GuiceServletModule extends ServletModule {
   protected void configureServlets() {
     filterRegex("/api/v0/.*").through(OAuthFilter.class);
     serve("/api/v0/layer/.*").with(LayerServlet.class);
+    serve("/api/v0/atom/.*").with(AtomServlet.class);
+    serve("/api/v0/search").with(SearchServlet.class);
+    serve("/layarPOI").with(LayarGetPointsOfInterestServlet.class);
   }
 }
