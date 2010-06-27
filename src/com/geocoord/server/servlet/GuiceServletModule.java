940dba45c9a6000d8858977f8c2e0a7038aecc58
hbs <hbs@f36f38b0-f878-0410-a4c7-8738c5d2aafc>
2010-06-27 21:38:37 +0000

    Added twitter/layar mapping.
    git-svn-id: svn+ssh://glop.iroise.net/GeoCoord/trunk@280 f36f38b0-f878-0410-a4c7-8738c5d2aafc

--- src/com/geocoord/server/servlet/GuiceServletModule.java
+++ src/com/geocoord/server/servlet/GuiceServletModule.java
@@ -3,15 +3,20 @@ package com.geocoord.server.servlet;
 import com.geocoord.server.servlet.api.AtomServlet;
 import com.geocoord.server.servlet.api.LayerServlet;
 import com.geocoord.server.servlet.api.SearchServlet;
+import com.geocoord.twitter.TwitterCallbackServlet;
+import com.geocoord.twitter.TwitterLoginServlet;
 import com.google.inject.servlet.ServletModule;
 
 public class GuiceServletModule extends ServletModule {
   @Override
   protected void configureServlets() {
-    filterRegex("/api/v0/.*").through(OAuthFilter.class);
+    filterRegex("/api/v0/(layer|atom|search).*").through(OAuthFilter.class);
     serve("/api/v0/layer/.*").with(LayerServlet.class);
     serve("/api/v0/atom/.*").with(AtomServlet.class);
     serve("/api/v0/search").with(SearchServlet.class);
-    serve("/layarPOI").with(LayarGetPointsOfInterestServlet.class);
+    serve("/api/v0/layar").with(LayarGetPointsOfInterestServlet.class);
+    serve("/api/v0/layar/.*").with(LayarGetPointsOfInterestServlet.class);
+    serve("/twitter/login").with(TwitterLoginServlet.class);
+    serve("/twitter/callback").with(TwitterCallbackServlet.class);
   }
 }
