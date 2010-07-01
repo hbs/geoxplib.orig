37205ebc267faeca6cafdbf8775005d4bd6981c7
hbs <hbs@f36f38b0-f878-0410-a4c7-8738c5d2aafc>
2010-07-01 22:03:41 +0000

    Added area field to sort Geofences.
    git-svn-id: svn+ssh://glop.iroise.net/GeoCoord/trunk@294 f36f38b0-f878-0410-a4c7-8738c5d2aafc

--- src/com/geocoord/lucene/GeoScoreDoc.java
+++ src/com/geocoord/lucene/GeoScoreDoc.java
@@ -7,12 +7,22 @@ public class GeoScoreDoc extends ScoreDoc {
   private final long uuidLsb;
   private final long uuidMsb;
   
+  public final long area;
+  
   public GeoScoreDoc(int doc, float score, long uuidLsb, long uuidMsb) {
     super(doc, score);
     this.uuidLsb = uuidLsb;
     this.uuidMsb = uuidMsb;
+    this.area = 0;
   }
-  
+
+  public GeoScoreDoc(int doc, float score, long uuidLsb, long uuidMsb, long area) {
+    super(doc, score);
+    this.uuidLsb = uuidLsb;
+    this.uuidMsb = uuidMsb;
+    this.area = area;
+  }
+
   public long getUuidLsb() {
     return uuidLsb;
   }
