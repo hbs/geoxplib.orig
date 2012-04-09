10ffbfdc6684924bea8b9176e49de55f690f7a38
hbs <hbs@f36f38b0-f878-0410-a4c7-8738c5d2aafc>
2012-04-09 20:43:45 +0000

    Added /heatmap
    git-svn-id: svn+ssh://glop.iroise.net/GeoCoord/trunk@385 f36f38b0-f878-0410-a4c7-8738c5d2aafc

--- src/com/geocoord/server/servlet/GuiceServletModule.java
+++ src/com/geocoord/server/servlet/GuiceServletModule.java
@@ -5,11 +5,15 @@ import com.geocoord.server.servlet.api.LayerServlet;
 import com.geocoord.server.servlet.api.SearchServlet;
 import com.geocoord.twitter.TwitterCallbackServlet;
 import com.geocoord.twitter.TwitterLoginServlet;
+import com.geoxp.heatmap.HeatMapManagerTileBuilder;
+import com.geoxp.heatmap.TileServlet;
 import com.google.inject.servlet.ServletModule;
 
 public class GuiceServletModule extends ServletModule {
   @Override
   protected void configureServlets() {
+    serve("/heatmap").with(TileServlet.class);
+    /*
     filterRegex("/api/v0/(layer|atom|search)/(.)*").through(OAuthFilter.class);
     serveRegex("/api/v0/layer/(.)*").with(LayerServlet.class);
     serve("/api/v0/atom/search").with(SearchServlet.class);
@@ -19,5 +23,6 @@ public class GuiceServletModule extends ServletModule {
     serveRegex("/api/v0/layar/(.)*").with(LayarGetPointsOfInterestServlet.class);
     serve("/twitter/login").with(TwitterLoginServlet.class);
     serve("/twitter/callback").with(TwitterCallbackServlet.class);
+    */
   }
 }
