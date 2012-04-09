25a1a5163f4421dea9083955604b76abc86c7359
hbs <hbs@f36f38b0-f878-0410-a4c7-8738c5d2aafc>
2012-04-09 20:44:08 +0000

    Initial revision, experimental.
    git-svn-id: svn+ssh://glop.iroise.net/GeoCoord/trunk@386 f36f38b0-f878-0410-a4c7-8738c5d2aafc

---
+++ src/org/apache/lucene/index/GeoXPConcurrentMergeScheduler.java
@@ -0,0 +1,31 @@
+package org.apache.lucene.index;
+
+import java.io.IOException;
+
+import org.apache.lucene.index.MergePolicy.OneMerge;
+
+import com.geocoord.lucene.GeoDataSegmentCache;
+
+public class GeoXPConcurrentMergeScheduler extends ConcurrentMergeScheduler {
+  
+  private final IndexWriter writer;
+  
+  public GeoXPConcurrentMergeScheduler(IndexWriter writer) {
+    this.writer = writer;
+  }
+  
+  @Override
+  protected void doMerge(OneMerge merge) throws IOException {
+    super.doMerge(merge);
+    
+    // 
+    // Reflect the merge in the segment cache.
+    // We need to do this otherwise deleteByUUID might miss deletes by not
+    // finding atoms in the known segments.
+    // This is why deleteByUUID calls waitForMerges, because when waitForMerges
+    // returns, GeoDataSegmentCache.commitMerge will have been called and the
+    // merged segments will have been replaced by the resulting single segment.
+    //
+    GeoDataSegmentCache.commitMerge(this.writer, merge.segments, merge.info);
+  }
+}
