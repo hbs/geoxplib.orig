d14f71bfba757c24e05e5adbc7c7c83d3663175b
hbs <hbs@f36f38b0-f878-0410-a4c7-8738c5d2aafc>
2013-01-22 21:50:20 +0000

    Moved in main/java
    git-svn-id: svn+ssh://glop.iroise.net/GeoCoord/trunk@419 f36f38b0-f878-0410-a4c7-8738c5d2aafc

--- src/com/geoxp/client/widgets/poipanel/i18n/images/PoiPanelImages.java
+++
@@ -1,7 +0,0 @@
-package com.geoxp.client.widgets.poipanel.i18n.images;
-
-import com.google.gwt.resources.client.ClientBundle;
-
-public interface PoiPanelImages extends ClientBundle {
-
-}
--- src/com/geoxp/util/CryptoHelper.java
+++
@@ -1,7 +0,0 @@
-package com.geoxp.util;
-
-import java.security.SecureRandom;
-
-public interface CryptoHelper {
-  public SecureRandom getSecureRandom();
-}
