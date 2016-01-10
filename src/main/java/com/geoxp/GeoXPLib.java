65855c5044298193cdc6258f9e2d0a03552621a6
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2016-01-10 23:48:33 +0100

    Cleaned up

--- src/main/java/com/geoxp/GeoXPLib.java
+++ src/main/java/com/geoxp/GeoXPLib.java
@@ -19,35 +19,13 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-//
-//  GeoXP Lib, library for efficient geo data manipulation
-//
-//  Copyright (C) 1999-2016  Mathias Herberts
-//
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Affero General Public License as
-//  published by the Free Software Foundation, either version 3 of the
-//  License, or (at your option) any later version and under the terms
-//  of the GeoXP License Exception.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Affero General Public License for more details.
-//
-//  You should have received a copy of the GNU Affero General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-
 package com.geoxp;
 
 import java.io.Serializable;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.Collection;
-
-import org.bouncycastle.util.Arrays;
+import java.util.Arrays;
 
 import com.geoxp.geo.Coverage;
 import com.geoxp.geo.CoverageHelper;
