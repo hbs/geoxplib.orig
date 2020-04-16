5295f2a76036ae65a6c35f3660ed629463995f5c
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2020-04-16 15:58:37 +0200

    Modified copyright notice

--- src/main/java/com/geoxp/geo/JTSHelper.java
+++ src/main/java/com/geoxp/geo/JTSHelper.java
@@ -1,27 +1,24 @@
 //
-//  GeoXP Lib, library for efficient geo data manipulation
+//   GeoXP Lib, library for efficient geo data manipulation
 //
-//  Copyright (C) 1999-2016  Mathias Herberts
+//   Copyright 2020-      SenX S.A.S.
+//   Copyright 2019-2020  iroise.net S.A.S.
+//   Copyright 1999-2019  Mathias Herberts
 //
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
 //
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Affero General Public License as
-//  published by the Free Software Foundation, either version 3 of the
-//  License, or (at your option) any later version and under the terms
-//  of the GeoXP License Exception.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Affero General Public License for more details.
-//
-//  You should have received a copy of the GNU Affero General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
 //
 
 package com.geoxp.geo;
-
-import gnu.trove.list.array.TLongArrayList;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
